bee72fff0460b253c1487bbca349d2d87e4cfd70
opalreeves <opalmreeves@gmail.com>
2025-10-03 20:51:03 -0700

    adding to repository

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Opal Reeves
+# GitHub username: opalreeves
+# Date: 10/2/2025
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
