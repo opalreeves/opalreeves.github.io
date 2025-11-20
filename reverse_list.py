99bb1c6a1f2fb5c95a629f35560c88f8921e9756
opalreeves <opalmreeves@gmail.com>
2025-11-19 23:20:48 -0800

    removed test cases

--- reverse_list.py
+++ reverse_list.py
@@ -6,7 +6,3 @@ def reverse_list(vals):
         vals[left], vals[right] = vals[right], vals[left]
         left += 1
         right -= 1
-
-# vals = [7, -3, 12, 9]
-# reverse_list(vals)
-# print(vals)  # This should print [9, 12, -3, 7]
--- square_list.py
+++ square_list.py
@@ -1,7 +1,3 @@
 def square_list(nums):
     for i in range(len(nums)):
         nums[i] = nums[i] ** 2
-
-# nums = [7, -3, 12, 9]
-# square_list(nums)
-# print(nums)  # This should print [49, 9, 144, 81]
