fc9e3e05fc2e51426dd1dd895681e6e16e5e3691
opalreeves <opalmreeves@gmail.com>
2025-11-19 22:30:10 -0800

    created square lists

---
+++ square_list.py
@@ -0,0 +1,7 @@
+def square_list(nums):
+    for i in range(len(nums)):
+        nums[i] = nums[i] ** 2
+
+# nums = [7, -3, 12, 9]
+# square_list(nums)
+# print(nums)  # This should print [49, 9, 144, 81]
