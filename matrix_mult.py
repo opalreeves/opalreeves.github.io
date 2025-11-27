25b345aca1d9f03cab5fb0a5e50d95b9792ea71f
opalreeves <opalmreeves@gmail.com>
2025-11-26 16:05:50 -0800

    matrix multiplication

---
+++ matrix_mult.py
@@ -0,0 +1,38 @@
+def dot_product(a, b):
+    if len(a) != len(b):
+        return None
+    
+    total = 0
+    for i in range(len(a)):
+        total += a[i] * b[i]
+    return total
+
+def matrix_mult(A, B):
+    if len(A[0]) != len(B):
+        return None
+    
+    result = []
+
+    num_rows_A = len(A)
+    num_colums_B = len(B[0])
+
+    for i in range(num_rows_A):
+        new_row = []
+        
+        for j in range(num_colums_B):
+            column = []
+            
+            for k in range(len(B)):
+                column.append(B[k][j])
+
+            value = dot_product(A[i], column)
+            new_row.append(value)
+
+        result.append(new_row)
+    return result
+
+#if __name__ == "__main__":
+    A = [[3, 1], [-8, 5], [1, 4]]
+    B = [[1, 4, 7, 1], [-5, -8, 4, 3]]
+
+    print(matrix_mult(A, B))
